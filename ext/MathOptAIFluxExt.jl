4858fb8f65d1dc78ec5800a904dc7f221730d5bf
Oscar Dowson <odow@users.noreply.github.com>
2024-06-07 19:42:57 +1200

    Add Flux.jl extension (#25)

---
+++ ext/MathOptAIFluxExt.jl
@@ -0,0 +1,111 @@
+# Copyright (c) 2024: Oscar Dowson and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
+module MathOptAIFluxExt
+
+import Flux
+import JuMP
+import MathOptAI
+
+_default(::typeof(identity)) = nothing
+_default(::Any) = missing
+_default(::typeof(Flux.relu)) = MathOptAI.ReLU()
+_default(::typeof(Flux.sigmoid)) = MathOptAI.Sigmoid()
+_default(::typeof(Flux.softplus)) = MathOptAI.SoftPlus()
+_default(::typeof(Flux.tanh)) = MathOptAI.Tanh()
+
+function _add_predictor(
+    predictor::MathOptAI.Pipeline,
+    activation::Function,
+    config::Dict{<:Function,<:MathOptAI.AbstractPredictor},
+)
+    layer = get(config, activation, _default(activation))
+    if layer === nothing
+        # Do nothing: a linear activation
+    elseif layer === missing
+        error("Unsupported activation function: $activation")
+    else
+        push!(predictor.layers, layer)
+    end
+    return
+end
+
+function _add_predictor(
+    predictor::MathOptAI.Pipeline,
+    layer::Flux.Dense,
+    config::Dict{<:Function,<:MathOptAI.AbstractPredictor},
+)
+    push!(
+        predictor.layers,
+        MathOptAI.LinearRegression(layer.weight, layer.bias),
+    )
+    _add_predictor(predictor, layer.σ, config)
+    return
+end
+
+"""
+    MathOptAI.add_predictor(
+        model::JuMP.Model,
+        predictor::Flux.Chain,
+        x::Vector{JuMP.VariableRef};
+        config::Dict{<:Function,<:MathOptAI.AbstractPredictor} = Dict(),
+    )
+
+Add a trained neural network from Flux.jl to `model`.
+
+## Supported layers
+
+ * `Flux.Dense`
+
+## Supported activation funnctions
+
+ * `Flux.relu`
+ * `Flux.sigmoid`
+ * `Flux.softplus`
+ * `Flux.tanh`
+
+## Keyword arguments
+
+ * `config`: a dictionary that maps `Flux` activation functions to an
+   `AbstractPredictor` to control how the activation functions are reformulated.
+
+## Example
+
+```jldoctest
+julia> using JuMP, Flux, MathOptAI
+
+julia> chain = Flux.Chain(Flux.Dense(1 => 16, Flux.relu), Flux.Dense(16 => 1));
+
+julia> model = Model();
+
+julia> @variable(model, x[1:1]);
+
+julia> y = MathOptAI.add_predictor(
+           model,
+           chain,
+           x;
+           config = Dict(Flux.relu => MathOptAI.ReLU()),
+       )
+1-element Vector{VariableRef}:
+ omelette_LinearRegression[1]
+```
+"""
+function MathOptAI.add_predictor(
+    model::JuMP.Model,
+    predictor::Flux.Chain,
+    x::Vector{JuMP.VariableRef};
+    config::Dict{<:Function,<:MathOptAI.AbstractPredictor} = Dict{
+        Function,
+        MathOptAI.AbstractPredictor,
+    }(),
+)
+    inner_predictor = MathOptAI.Pipeline(MathOptAI.AbstractPredictor[])
+    for layer in predictor.layers
+        _add_predictor(inner_predictor, layer, config)
+    end
+    return MathOptAI.add_predictor(model, inner_predictor, x)
+end
+
+end  # module
---
+++ test/test_Flux.jl
@@ -0,0 +1,153 @@
+# Copyright (c) 2024: Oscar Dowson and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
+module FluxTests
+
+using JuMP
+using Test
+
+import Flux
+import HiGHS
+import Ipopt
+import MathOptAI
+import Random
+
+is_test(x) = startswith(string(x), "test_")
+
+function runtests()
+    @testset "$name" for name in filter(is_test, names(@__MODULE__; all = true))
+        getfield(@__MODULE__, name)()
+    end
+    return
+end
+
+function generate_data(rng::Random.AbstractRNG, n = 128)
+    x = range(-2.0f0, 2.0f0, n)
+    y = -2 .* x .+ x .^ 2 .+ 0.1f0 .* randn(rng, n)
+    return [([xi], yi) for (xi, yi) in zip(x, y)]
+end
+
+function _train_lux_model(model)
+    rng = Random.MersenneTwister()
+    Random.seed!(rng, 12345)
+    data = generate_data(rng)
+    optim = Flux.setup(Flux.Adam(0.01f0), model)
+    for epoch in 1:250
+        Flux.train!((m, x, y) -> (only(m(x)) - y)^2, model, data, optim)
+    end
+    return model
+end
+
+function test_end_to_end_ReLUBigM()
+    chain = _train_lux_model(
+        Flux.Chain(Flux.Dense(1 => 16, Flux.relu), Flux.Dense(16 => 1)),
+    )
+    model = Model(HiGHS.Optimizer)
+    set_silent(model)
+    @variable(model, x)
+    y = MathOptAI.add_predictor(
+        model,
+        chain,
+        [x];
+        config = Dict(Flux.relu => MathOptAI.ReLUBigM(100.0)),
+    )
+    @constraint(model, only(y) <= 4)
+    @objective(model, Min, x)
+    optimize!(model)
+    @test is_solved_and_feasible(model)
+    @test isapprox(value(x), -1.24; atol = 1e-1)
+    return
+end
+
+function test_end_to_end_ReLUQuadratic()
+    chain = _train_lux_model(
+        Flux.Chain(Flux.Dense(1 => 16, Flux.relu), Flux.Dense(16 => 1)),
+    )
+    model = Model(Ipopt.Optimizer)
+    set_silent(model)
+    @variable(model, x)
+    y = MathOptAI.add_predictor(
+        model,
+        chain,
+        [x];
+        config = Dict(Flux.relu => MathOptAI.ReLUQuadratic()),
+    )
+    # Ipopt needs a starting point to avoid the local minima.
+    set_start_value(only(y), 4.0)
+    @constraint(model, only(y) <= 4)
+    @objective(model, Min, x)
+    optimize!(model)
+    @test is_solved_and_feasible(model)
+    @test isapprox(value.(y), chain([value(x)]); atol = 1e-2)
+    return
+end
+
+function test_end_to_end_ReLU()
+    chain = _train_lux_model(
+        Flux.Chain(Flux.Dense(1 => 16, Flux.relu), Flux.Dense(16 => 1)),
+    )
+    model = Model(Ipopt.Optimizer)
+    set_silent(model)
+    @variable(model, x)
+    y = MathOptAI.add_predictor(model, chain, [x])
+    @constraint(model, only(y) <= 4)
+    @objective(model, Min, x)
+    optimize!(model)
+    @test is_solved_and_feasible(model)
+    @test isapprox(value.(y), chain([value(x)]); atol = 1e-2)
+    return
+end
+
+function test_end_to_end_SoftPlus()
+    chain = _train_lux_model(
+        Flux.Chain(Flux.Dense(1 => 16, Flux.softplus), Flux.Dense(16 => 1)),
+    )
+    model = Model(Ipopt.Optimizer)
+    set_silent(model)
+    @variable(model, x)
+    y = MathOptAI.add_predictor(model, chain, [x])
+    @constraint(model, only(y) <= 4)
+    @objective(model, Min, x)
+    optimize!(model)
+    @test is_solved_and_feasible(model)
+    @test isapprox(value.(y), chain([value(x)]); atol = 1e-2)
+    return
+end
+
+function test_end_to_end_Sigmoid()
+    chain = _train_lux_model(
+        Flux.Chain(Flux.Dense(1 => 16, Flux.sigmoid), Flux.Dense(16 => 1)),
+    )
+    model = Model(Ipopt.Optimizer)
+    set_silent(model)
+    @variable(model, x)
+    y = MathOptAI.add_predictor(model, chain, [x])
+    @constraint(model, only(y) <= 4)
+    @objective(model, Min, x)
+    optimize!(model)
+    @test is_solved_and_feasible(model)
+    @test isapprox(value.(y), chain([value(x)]); atol = 1e-2)
+    return
+end
+
+function test_end_to_end_Tanh()
+    chain = _train_lux_model(
+        Flux.Chain(Flux.Dense(1 => 16, Flux.tanh), Flux.Dense(16 => 1)),
+    )
+    model = Model(Ipopt.Optimizer)
+    set_silent(model)
+    @variable(model, x)
+    y = MathOptAI.add_predictor(model, chain, [x])
+    @constraint(model, only(y) <= 4)
+    @objective(model, Min, x)
+    optimize!(model)
+    @test is_solved_and_feasible(model)
+    @test isapprox(value.(y), chain([value(x)]); atol = 1e-2)
+    return
+end
+
+end  # module
+
+FluxTests.runtests()

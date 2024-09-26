0901b31700726c477d6e7e964af299e57ede5014
Robert Parker <Robbybp@users.noreply.github.com>
2024-09-25 22:37:31 -0600

    Support `beta` parameter in SoftPlus (#114)
    * support beta in SoftPlus
    
    * update docs
    
    * support beta in Softplus from torch
    
    * remove type parameter from softplus
    
    * softplus default constructor uses beta keywd
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * revert order of softmax/softplus
    
    ---------
    
    Co-authored-by: Oscar Dowson <odow@users.noreply.github.com>

--- src/predictors/SoftPlus.jl
+++ src/predictors/SoftPlus.jl
@@ -5,10 +5,10 @@
 # in the LICENSE.md file.
 
 """
-    SoftPlus() <: AbstractPredictor
+    SoftPlus(; beta = 1.0) <: AbstractPredictor
 
 An [`AbstractPredictor`](@ref) that implements the SoftPlus constraint
-\$y = \\log(1 + e^x)\$ as a smooth nonlinear constraint.
+\$y = \\frac{1}{\\beta} \\log(1 + e^{\\beta x})\$ as a smooth nonlinear constraint.
 
 ## Example
 
@@ -19,8 +19,8 @@ julia> model = Model();
 
 julia> @variable(model, x[1:2]);
 
-julia> f = MathOptAI.SoftPlus()
-SoftPlus()
+julia> f = MathOptAI.SoftPlus(; beta = 2.0)
+SoftPlus(2.0)
 
 julia> y, formulation = MathOptAI.add_predictor(model, f, x);
 
@@ -30,31 +30,34 @@ julia> y
  moai_SoftPlus[2]
 
 julia> formulation
-SoftPlus()
+SoftPlus(2.0)
 ├ variables [2]
 │ ├ moai_SoftPlus[1]
 │ └ moai_SoftPlus[2]
 └ constraints [4]
   ├ moai_SoftPlus[1] ≥ 0
   ├ moai_SoftPlus[2] ≥ 0
-  ├ moai_SoftPlus[1] - log(1.0 + exp(x[1])) = 0
-  └ moai_SoftPlus[2] - log(1.0 + exp(x[2])) = 0
+  ├ moai_SoftPlus[1] - (log(1.0 + exp(2 x[1])) / 2.0) = 0
+  └ moai_SoftPlus[2] - (log(1.0 + exp(2 x[2])) / 2.0) = 0
 
 julia> y, formulation =
            MathOptAI.add_predictor(model, MathOptAI.ReducedSpace(f), x);
 
 julia> y
 2-element Vector{NonlinearExpr}:
- log(1.0 + exp(x[1]))
- log(1.0 + exp(x[2]))
+ log(1.0 + exp(2 x[1])) / 2.0
+ log(1.0 + exp(2 x[2])) / 2.0
 
 julia> formulation
-ReducedSpace(SoftPlus())
+ReducedSpace(SoftPlus(2.0))
 ├ variables [0]
 └ constraints [0]
 ```
 """
-struct SoftPlus <: AbstractPredictor end
+struct SoftPlus <: AbstractPredictor
+    beta::Float64
+    SoftPlus(; beta::Float64 = 1.0) = new(beta)
+end
 
 function add_predictor(
     model::JuMP.AbstractModel,
@@ -63,7 +66,8 @@ function add_predictor(
 )
     y = JuMP.@variable(model, [1:length(x)], base_name = "moai_SoftPlus")
     _set_bounds_if_finite.(y, 0, nothing)
-    cons = JuMP.@constraint(model, y .== log.(1 .+ exp.(x)))
+    beta = predictor.beta
+    cons = JuMP.@constraint(model, y .== log.(1 .+ exp.(beta .* x)) ./ beta)
     return y, Formulation(predictor, y, Any[JuMP.LowerBoundRef.(y); cons])
 end
 
@@ -72,5 +76,6 @@ function add_predictor(
     predictor::ReducedSpace{SoftPlus},
     x::Vector,
 )
-    return log.(1 .+ exp.(x)), Formulation(predictor)
+    beta = predictor.predictor.beta
+    return log.(1 .+ exp.(beta .* x)) ./ beta, Formulation(predictor)
 end

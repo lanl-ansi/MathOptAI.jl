2e4f153fc99da4fdd27fb224a5d8da5d62cb5342
Oscar Dowson <odow@users.noreply.github.com>
2026-06-16 15:54:55 +1200

    Update MathOptAI.jl

--- src/MathOptAI.jl
+++ src/MathOptAI.jl
@@ -358,6 +358,7 @@ output_size(::AbstractPredictor, ::Any) = nothing
 
 for file in readdir(joinpath(@__DIR__, "predictors"); join = true)
     if endswith(file, ".jl")
+        @show file
         include(file)
     end
 end

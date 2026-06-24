6e73ab1d5bd28109e8f02e75d929a7e8874270fc
Oscar Dowson <odow@users.noreply.github.com>
2026-06-24 13:45:14 +1200

    [docs] tidy the Input Convex Neural Network tutorial (#280)

--- docs/src/tutorials/input_convex.jl
+++ docs/src/tutorials/input_convex.jl
@@ -15,7 +15,9 @@
 
 using JuMP
 import Flux
+import HiGHS
 import MathOptAI
+import Plots
 
 # ## Building the ICNN
 
@@ -30,7 +32,7 @@ struct InputConvex{T,F}
     σ::F
 end
 
-Flux.@layer InputConvex trainable=(weight_x, weight_z, bias)
+Flux.@layer(InputConvex, trainable = (weight_x, weight_z, bias))
 
 function InputConvex(
     ((in_z, in_x), out)::Pair{Tuple{Int,Int},Int},
@@ -40,33 +42,34 @@ function InputConvex(
     return InputConvex(init(out, in_x), init(out, in_z), init(out), σ)
 end
 
-function (c::InputConvex)(x)
+function (c::InputConvex)(x::AbstractVector)
     return c.σ.(c.weight_x * x .+ c.bias), x
 end
 
-function (c::InputConvex)(input::Tuple)
-    z, x = input
+function (c::InputConvex)((z, x)::Tuple)
     return c.σ.(Flux.softplus.(c.weight_z) * z .+ c.weight_x * x .+ c.bias), x
 end
 
 function Base.show(io::IO, l::InputConvex)
-    print(
-        io,
-        "InputConvex((",
-        size(l.weight_z, 2),
-        ", ",
-        size(l.weight_x, 2),
-        ") => ",
-        size(l.weight_x, 1),
-    )
+    m, n = size(l.weight_x)
+    print(io, "InputConvex((", size(l.weight_z, 2), ", $m) => $n")
     if l.σ != identity
         print(io, ", ", l.σ)
     end
     if l.bias == false
         print(io, "; bias=false")
     end
-    return print(io, ")")
+    print(io, ")")
+    return
 end
+
+# Here's an example:
+
+layer = InputConvex((8, 8) => 2, Flux.relu)
+
+#-
+
+layer(rand(8))
 
 # Next, we define a custom `Chain` to build the ICNN.
 
@@ -81,8 +84,20 @@ InputConvexChain(layers...) = InputConvexChain(Flux.Chain(layers))
 function Base.show(io::IO, l::InputConvexChain)
     println(io, "InputConvexChain(")
     println.(io, "\t", l.chain)
-    return println(io, ")")
+    println(io, ")")
+    return
 end
+
+# Here's an example:
+
+chain = InputConvexChain(
+    InputConvex((8, 8) => 2, Flux.relu),
+    InputConvex((2, 8) => 1, Flux.relu),
+)
+
+#-
+
+chain(rand(8))
 
 # ## Building the Predictor
 
@@ -96,9 +111,7 @@ end
 function MathOptAI.build_predictor(
     predictor::InputConvexChain;
     config::Dict = Dict{Any,Any}(),
-    gray_box::Bool = false,
-    hessian::Bool = gray_box,
-    input_size::Union{Nothing,NTuple} = nothing,
+    kwargs...,
 )
     layer1 = first(predictor.chain)
     p = MathOptAI.Pipeline(
@@ -106,13 +119,8 @@ function MathOptAI.build_predictor(
         MathOptAI.build_predictor(layer1.σ; config),
     )
     for layer in predictor.chain[2:end]
-        push!(
-            p.layers,
-            MathOptAI.Affine(
-                [Flux.softplus(layer.weight_z) layer.weight_x],
-                layer.bias,
-            ),
-        )
+        weights = hcat(Flux.softplus(layer.weight_z), layer.weight_x)
+        push!(p.layers, MathOptAI.Affine(weights, layer.bias))
         push!(p.layers, MathOptAI.build_predictor(layer.σ; config))
     end
     return InputConvexChainPredictor(p)
@@ -120,22 +128,20 @@ end
 
 function MathOptAI.add_predictor(
     model::JuMP.AbstractModel,
-    predictor::InputConvexChain,
+    predictor::InputConvexChainPredictor,
     x::Vector;
     kwargs...,
-)::Tuple{<:Vector,<:MathOptAI.AbstractFormulation}
-    predictor = MathOptAI.build_predictor(predictor; kwargs...)
-    layer1 = first(predictor.p.layers)
-    formulation = MathOptAI.PipelineFormulation(predictor, [])
-    z, inner_formulation = MathOptAI.add_predictor(model, layer1, x)
-    push!(formulation.layers, inner_formulation)
-    for layer in predictor.p.layers[2:end]
-        z, inner_formulation = if layer isa MathOptAI.Affine
+)
+    layers = predictor.p.layers
+    z, inner = MathOptAI.add_predictor(model, first(layers), x)
+    formulation = MathOptAI.PipelineFormulation(predictor, Any[inner])
+    for layer in layers[2:end]
+        z, inner = if layer isa MathOptAI.Affine
             MathOptAI.add_predictor(model, layer, [z; x])
         else
             MathOptAI.add_predictor(model, layer, z)
         end
-        push!(formulation.layers, inner_formulation)
+        push!(formulation.layers, inner)
     end
     return z, formulation
 end
@@ -170,23 +176,29 @@ z
 
 formulation
 
+# ## Epigraph formulations
+
 # The nice thing about ICNNs is that we can formulate their epigraph and avoid
 # adding binary variables to the model. For that, we can use
 # [`ReLUEpigraph`](@ref).
 
-model = Model();
-@variable(model, x[1:8]);
-z, formulation = MathOptAI.add_predictor(
-    model,
-    predictor,
-    x;
-    config = Dict(Flux.relu => MathOptAI.ReLUEpigraph),
-);
+chain = InputConvexChain(
+    InputConvex((1, 1) => 3, Flux.relu),
+    InputConvex((3, 1) => 1, Flux.relu),
+)
+model = Model(HiGHS.Optimizer)
+set_silent(model)
+@variable(model, x[1:1])
+config = Dict(Flux.relu => MathOptAI.ReLUEpigraph)
+y, _ = MathOptAI.add_predictor(model, chain, x; config)
+@objective(model, Min, only(y))
+x_value, y_value = -20:20, Float64[]
+for xi in x_value
+    fix(x[1], xi)
+    optimize!(model)
+    assert_is_solved_and_feasible(model)
+    push!(y_value, objective_value(model))
+end
+Plots.plot(x_value, y_value)
 
-#-
-
-z
-
-#-
-
-formulation
+# As expected, the value of `y` is convex with respect to `x`.

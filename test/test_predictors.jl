381b8bffec894aba67f8a8f63c62a65e86d73da9
Gökhan Kof <kofgokhan@gmail.com>
2026-07-02 12:42:42 +0300

    Fix format

--- test/test_predictors.jl
+++ test/test_predictors.jl
@@ -457,8 +457,10 @@ function test_SoftPlusConicEpigraph()
     model = Model(SCS.Optimizer)
     set_silent(model)
     @variable(model, x[1:2])
-    y, formulation = MathOptAI.add_predictor(model, MathOptAI.SoftPlusConicEpigraph(), x)
-    @test MathOptAI.output_size(MathOptAI.SoftPlusConicEpigraph(), (10,)) == (10,)
+    y, formulation =
+        MathOptAI.add_predictor(model, MathOptAI.SoftPlusConicEpigraph(), x)
+    @test MathOptAI.output_size(MathOptAI.SoftPlusConicEpigraph(), (10,)) ==
+          (10,)
     @test length(y) == 2
     @test num_variables(model) == 8
     @test num_constraints(model, NonlinearExpr, MOI.EqualTo{Float64}) == 0
@@ -477,8 +479,13 @@ function test_SoftPlusConicEpigraph_beta4()
     model = Model(SCS.Optimizer)
     set_silent(model)
     @variable(model, x[1:2])
-    y, formulation = MathOptAI.add_predictor(model, MathOptAI.SoftPlusConicEpigraph(;beta=beta), x)
-    @test MathOptAI.output_size(MathOptAI.SoftPlusConicEpigraph(), (10,)) == (10,)
+    y, formulation = MathOptAI.add_predictor(
+        model,
+        MathOptAI.SoftPlusConicEpigraph(; beta = beta),
+        x,
+    )
+    @test MathOptAI.output_size(MathOptAI.SoftPlusConicEpigraph(), (10,)) ==
+          (10,)
     @test length(y) == 2
     @test num_variables(model) == 8
     @test num_constraints(model, NonlinearExpr, MOI.EqualTo{Float64}) == 0

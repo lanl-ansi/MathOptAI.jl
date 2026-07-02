9c795906c3f8b52b0e0b0f4eed50a9a750851eb0
Oscar Dowson <odow@users.noreply.github.com>
2026-07-03 11:10:21 +1200

    Apply suggestion from @odow

--- test/test_predictors.jl
+++ test/test_predictors.jl
@@ -458,7 +458,7 @@ function test_SoftPlusConicEpigraph()
     set_silent(model)
     @variable(model, x[1:2])
     predictor = MathOptAI.SoftPlusConicEpigraph()
-    y, formulation = MathOptAI.add_predictor(model, , x)
+    y, formulation = MathOptAI.add_predictor(model, predictor, x)
     @test MathOptAI.output_size(predictor, (10,)) == (10,)
     @test length(y) == 2
     @test num_variables(model) == 8

d48f2f8a2052550e2c6aacf35456d13efe466aa5
Gökhan Kof <kofgokhan@gmail.com>
2026-07-21 00:10:40 +0300

    Add ReLUEpigraph for ExaModels (#294)

---
+++ ext/ExaModels/ReLUEpigraph.jl
@@ -0,0 +1,36 @@
+# Copyright (c) 2024: Triad National Security, LLC
+# Copyright (c) 2024: Oscar Dowson and contributors
+#
+# Use of this source code is governed by a BSD-style license that can be found
+# in the LICENSE.md file.
+
+function MathOptAI.add_predictor(
+    core::ExaModels.ExaCore,
+    p::MathOptAI.ReLUEpigraph,
+    x::ExaModels.AbstractVariable,
+)
+    n = _length(x)
+    core, y = ExaModels.add_var(core, n; lvar = 0.0)
+    core, c1 = ExaModels.add_con(
+        core,
+        y[i] - x[i] for i in 1:n;
+        lcon = 0.0,
+        ucon = Inf,
+    )
+    return (core, y), MathOptAI.Formulation(p, Any[y], Any[c1])
+end
+
+function MathOptAI.add_predictor(
+    core::ExaModels.ExaCore,
+    p::MathOptAI.ReLUEpigraph,
+    x::AbstractVector,
+)
+    n = length(x)
+    core, y = ExaModels.add_var(core, n; lvar = 0.0)
+    cons = Any[]
+    for i in 1:n
+        core, c = ExaModels.add_con(core, y[i] - x[i]; lcon = 0.0, ucon = Inf)
+        push!(cons, c)
+    end
+    return (core, y), MathOptAI.Formulation(p, Any[y], cons)
+end

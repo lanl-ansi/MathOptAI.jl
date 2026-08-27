37726080b907d162f9e318a4d37151c29b9142f6
Oscar Dowson <o.dowson@gmail.com>
2026-08-27 12:48:49 +1200

    Fix formatting

--- ext/ExaModels/SoftPlus.jl
+++ ext/ExaModels/SoftPlus.jl
@@ -27,9 +27,7 @@ function MathOptAI.add_predictor(
     x::Union{ExaModels.Variable,ExaModels.Expression},
 )
     β = p.predictor.beta
-    core, y = ExaModels.add_expr(
-        core,
-        log(1 + exp(β * x[i])) / β for i in 1:x.length
-    )
+    core, y =
+        ExaModels.add_expr(core, log(1 + exp(β * x[i])) / β for i in 1:x.length)
     return (core, y), MathOptAI.Formulation(p)
 end
